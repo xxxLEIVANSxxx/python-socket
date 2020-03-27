a165269e87220c13135e4718bc72789197c4ddc4
xxxLEIVANSxxx <samboeira@gmail.com>
2020-03-27 15:32:22 -0300

    Alterado para chat

--- client.py
+++ client.py
@@ -1,34 +1,31 @@
 import socket
-import time
 
-# Criar o socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = input("PORTA(ex:9000) >> ")
+print("")
 
-# Conectar ao servidor com ip e porta
-sock.connect(('localhost', 9000))
+aux = True
+while aux:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('localhost', int(port)))
 
-# Leia o tamanho da mensagem
-expected_data_size = int(sock.recv(4).decode())
-print("Tamanho de dado esperado = {}".format(expected_data_size))
+    expected_data_size = int(sock.recv(4).decode())
 
-received_data = ''
-while len(received_data) < expected_data_size:
-    # Ler o dado recebido
-    received_data += sock.recv(4).decode()
-    print("Tamanho do dado {}".format(len(received_data)))
-print(received_data)
+    received_data = ''
+    while len(received_data) < expected_data_size:
+        received_data += sock.recv(4).decode()
+    print("Resposta do servidor: " + received_data)
+    
+    if received_data == "see ya":
+        aux = False
+    else:
+        mensagem = input("MSG >> ")
+        send_data_size = len(mensagem)
+        sock.sendall(str(send_data_size).zfill(4).encode())
 
+        sock.sendall(mensagem.encode())
+        if mensagem == "see ya":
+            aux = False
 
-# Tamanho da mensagem
-mensagem = "Ola servidor"
-send_data_size = len(mensagem)
-sock.sendall(str(send_data_size).zfill(4).encode())
+    sock.close()
 
-# Enviar a mensagem
-sock.sendall(mensagem.encode())
-
-#
-time.sleep(3)
-
-# Finalizar a conexao
-sock.close()
+print("Finalizando...")
--- server.py
+++ server.py
@@ -1,43 +1,41 @@
 import socket
-# from socket import *
 
-# Criar o socket
+port = input("PORTA(ex:9000) >> ")
+print("")
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = sock.bind(('localhost', int(port)))
 
-# Escutar a porta 9000
-server = sock.bind(('localhost', 9000))
-
-# Definir o limite de 1 conexao paralela
 sock.listen(1)
 
-# Aceitar uma conexao e finaliza-la
-mensagem = "Ola Cliente"
-tamanho_da_mensagem = len(mensagem)
-print("Tamanho da mensagem = {}".format(len(mensagem)))
+aux = True;
 
-while True:
-    # Aguardar uma conexao
-    print("Aguardando conexao")
+while aux:
+    mensagem = input("MSG >> ")
+    tamanho_da_mensagem = len(mensagem)
+
     connection, address_client = sock.accept()    
     
-    # Envio tamanho da mensagem
     connection.sendall(str(tamanho_da_mensagem).zfill(4).encode())
     
-    # Enviar mensagem
     connection.sendall(mensagem.encode())
 
-    # Aguardar tamanho da mensagem
-    expected_data_size = ''
-    while(expected_data_size == ''):
-        expected_data_size += connection.recv(4).decode()
-    expected_data_size = int(expected_data_size)
+    if mensagem == "see ya":
+        aux = False
+    else:
+        expected_data_size = ''
+        while(expected_data_size == ''):
+            expected_data_size += connection.recv(4).decode()
+        expected_data_size = int(expected_data_size)
 
-    received_data = ''
-    while len(received_data) < expected_data_size:
-        # Ler o dado recebido
-        received_data += connection.recv(4).decode()
-        print("Tamanho do dado {}".format(len(received_data)))
-    print(received_data)
-
-    # Finalizar a conexao
+        received_data = ''
+        while len(received_data) < expected_data_size:
+            received_data += connection.recv(4).decode()
+        print("Resposta do cliente: " + received_data)
+        
+        if received_data == "see ya":
+            aux = False;
     connection.close()
+
+print("Finalizando...");
+sock.close()
